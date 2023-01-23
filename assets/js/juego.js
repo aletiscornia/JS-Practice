a2a563f55cc838108456fc2676bd1414c24fcb4e
Ale <tiscorniaale@gmail.com>
2023-01-23 14:50:35 -0300

    get one card

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,6 +2,7 @@ let deck = [];
 const types = ['C', 'D', 'H', 'S'];
 const specials = ['A', 'J', 'Q', 'K']
 
+//Crea nuevo deck
 const newDeck = () => {
     for( let i = 2; i <= 10; i++ ) {
         for( let type of types ) {
@@ -14,10 +15,23 @@ const newDeck = () => {
             deck.push( spe + type )
         }
     }
-    console.log(deck);
+
     deck = _.shuffle( deck );
     console.log(deck);
     return deck
 }
 
 newDeck();
+
+//Pedir una carta
+const getOneCard = () => {
+    if ( deck.length === 0 ) {
+        throw 'The deck is empty'
+    }
+    const card = deck.pop()
+    console.log(deck);
+    console.log(card);
+    return card;
+}
+
+getOneCard();
