5f4906d3f5d53f73698844aef7c037285fc36989
Ale <tiscorniaale@gmail.com>
2023-01-24 04:25:20 -0300

    adding cards and btn functions

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,6 +2,16 @@ let deck = [];
 const types = ['C', 'D', 'H', 'S'];
 const specials = ['A', 'J', 'Q', 'K']
 
+let playerPoints = 0;
+let computerPoints = 0;
+
+//Referencias HTML
+const btnGet = document.querySelector('#btnGet');
+
+const playerCardsDiv = document.querySelector('#player-cards');
+const computerCardsDiv = document.querySelector('#computer-cards');
+const htmlPoints = document.querySelectorAll('small');
+
 //Crea nuevo deck
 const newDeck = () => {
     for( let i = 2; i <= 10; i++ ) {
@@ -29,19 +39,36 @@ const getOneCard = () => {
         throw 'The deck is empty'
     }
     const card = deck.pop()
-    console.log(deck);
-    console.log(card);
     return card;
 }
 
-// getOneCard();
-
 const cardValue = (card) => {
-    const value = card.substring(0,card.length - 1 );
+    const value = card.substring(0, card.length - 1 );
     return ( isNaN( value ) ) ?
                 ( value === 'A' ) ? 11 : 10
                 : value * 1;
 }
 
-const value = cardValue( getOneCard() );
-console.log( { value } );
+// const value = cardValue( getOneCard() );
+
+//Eventos
+btnGet.addEventListener('click', () => {
+    const card = getOneCard();
+    playerPoints = playerPoints + cardValue( card );
+    htmlPoints[0].innerText = playerPoints;
+
+    const cardImg = document.createElement('img');
+    cardImg.src = `/assets/cartas/${card}.png`
+    cardImg.classList.add('cards')
+    playerCardsDiv.append( cardImg );
+
+    if( playerPoints > 21 ) {
+        console.warn('Te pasaste de 21');
+        btnGet.disabled = true;
+    } else if ( playerPoints === 21 ) {
+        console.warn('21, genial!');
+        btnGet.disabled = true;
+
+    }
+});
+
